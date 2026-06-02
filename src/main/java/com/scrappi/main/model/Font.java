23697a5b65e82f71cf1356bc673c88b7cd05879f
Mohit-Singh-007 <msms789549@gmail.com>
2026-06-02 14:53:07 +0530

    initial commit

--- src/main/java/com/scrappi/main/model/Font.java
+++ src/main/java/com/scrappi/main/model/Font.java
@@ -3,7 +3,6 @@ package com.scrappi.main.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 
 @Entity
 @Getter
--- src/main/java/com/scrappi/main/services/ScanImpl.java
+++ src/main/java/com/scrappi/main/services/ScanImpl.java
@@ -3,7 +3,6 @@ package com.scrappi.main.services;
 import com.scrappi.main.dto.ScanReq;
 import com.scrappi.main.dto.ScanRes;
 
-import java.util.Optional;
 
 public interface ScanImpl {
     Long createScan(ScanReq req);
